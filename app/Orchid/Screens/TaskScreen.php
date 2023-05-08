1a0443f3ea5bcc3a890807b7f10306e8c60810cb
nardosteve <jackcale50@gmail.com>
2023-05-08 15:54:20 +0300

    Task::Upload Successful

---
+++ app/Orchid/Screens/TaskScreen.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Orchid\Screens;
+
+use Orchid\Screen\Screen;
+use Orchid\Screen\Fields\Input;
+use Orchid\Support\Facades\Layout;
+use Orchid\Screen\Actions\ModalToggle;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Orchid\Screen\TD;
+use Orchid\Screen\Actions\Button;
+
+class TaskScreen extends Screen
+{
+    /**
+     * Fetch data to be displayed on the screen.
+     *
+     * @return array
+     */
+    public function query(): iterable
+    {
+        return [
+            'tasks' => Task::latest()->get(),
+        ];
+    }
+
+    /**
+     * The name of the screen displayed in the header.
+     *
+     * @return string|null
+     */
+    public function name(): ?string
+    {
+        return 'Simple To-Do List';
+    }
+
+    /**
+     * The description is displayed on the user's screen under the heading
+     */
+    public function description(): ?string
+    {
+        return 'Add your thoughts';
+    }
+
+    /**
+     * The screen's action buttons.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
+    public function commandBar(): iterable
+    {
+        return [
+            ModalToggle::make('Add Task')
+            ->modal('taskModal')
+            ->method('create')
+            ->icon('plus'),
+        ];
+    }
+
+        /**
+         * @param \Illuminate\Http\Request $request
+         *
+         * @return void
+         */
+        public function create(Request $request)
+        {
+            // Validate form data, save task to database, etc.
+            $request->validate([
+                'task.name' => 'required|max:255',
+            ]);
+
+            $task = new Task();
+            $task->name = $request->input('task.name');
+            $task->save();
+        }
+
+    /**
+     * The screen's layout elements.
+     *
+     * @return \Orchid\Screen\Layout[]|string[]
+     */
+    public function layout(): iterable
+    {
+        return [
+            //Displays the table
+            Layout::table('tasks', [
+                TD::make('name'),
+
+                TD::make('Actions')
+                ->alignRight()
+                ->render(function (Task $task) {
+                    return Button::make('Delete Task')
+                        ->confirm('After deleting, the task will be gone forever.')
+                        ->method('delete', ['task' => $task->id]);
+                }),
+            ]),
+            //Displays the table
+    
+            //Modal popup
+            Layout::modal('taskModal', Layout::rows([
+                Input::make('task.name')
+                    ->title('Name')
+                    ->placeholder('Enter task name')
+                    ->help('The name of the task to be created.'),
+            ]))
+                ->title('Create Task')
+                ->applyButton('Add Task'),
+            //Modal popup
+        ];
+    }
+
+        /**
+         * @param Task $task
+         *
+         * @return void
+         */
+        public function delete(Task $task)
+        {
+            $task->delete();
+        }
+}
